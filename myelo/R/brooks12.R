cfff6a125a8cdf7ca46677598ff199afc33ec051
radivot <txr24@case.edu>
2012-12-13 16:46:45 -0500

    decent first draft of brook12 was added. gcsfDemo now tests it.

---
+++ myelo/R/brooks12.R
@@ -0,0 +1,92 @@
+#'Neutrophil dynamics model of Brooks et al 2012
+#'
+#'This function returns the right hand side of an ordinary differential delay
+#'equation model of neutrophil dynamics published by Brooks et al. in 2012 
+#'The intended use of this function is as an argument to \code{dede()} of the \code{deSolve} package.
+#'
+#' In this model Q = quiescent stem cells and N = neutrophils
+#'
+#'@param Time The current time.
+#'@param State Vector of current states. The elements of this vector must be
+#'named because within this function's definition is
+#'\code{with(as.list(State,Pars), {XXX} )} and code in XXX presupposes that it
+#'can call named elements as variables with those names.
+#'@param Pars Vector of parameter values with \emph{named} (see above)
+#'elements.
+#'@return A list of length 1 where the first list
+#'element is the vector of derivatives, i.e. the right hand side of the ODEs 
+#'@note This work was supported by the National Cancer Institute and Tufts
+#'Integrative Cancer Biology Program under U54CA149233-029689.
+#'@author Tom Radivoyevitch (\email{txr24@@case.edu})
+#'@seealso \code{\link{myelo-package}, \link{rcj12}}
+#'@references  Grace Brooks, Gabriel Provencher, Jinzhi Lei, Michael C. Mackey,
+#'Neutrophil dynamics after chemotherapy and G-CSF: The role of pharmacokinetics 
+#' in shaping the response, \emph{Journal of Theoretical Biology} 
+#' \bold{315} 97-109 (2012).
+#'@keywords IO
+#'@export
+#'@examples
+#'
+#'\dontrun{
+#'library(myelo)
+#' 
+#' times <- c(-20:-1,seq(-.1,1,by=0.02),2:14,seq(14.01,15,by=0.02),16:100)
+#' yout <- dede(c(Q=brooksPars[["Qss"]],N=brooksPars[["Nss"]],EtaNP=0,Gam0=0,GamS=0,G=0,X=0,C=0),
+#' 		times = times, func = brooks12,	parms = brooksPars)
+#' # this should make Figure 3A. The response appears to be off by a dose-amplitude parameter
+#' par(mfrow=c(1,1),mar=c(4.7,6,3.3,1),oma=c(0,0,0,0),lwd=3,cex.lab=1.8,cex.axis=1.7,cex.main=1.8)
+#' plot(times,yout[,3]/1e8,type="l",xlab="days",ylab="Neutrophils")
+#' 
+#'
+#'}
+#'
+
+brooks12<-function(Time, State, Pars) {
+	with(as.list(c(State, Pars)), {
+				gamSChemo=gamS+hS*C
+				etaNPChemo=etaNP-hNP*C
+				gamSv=gamMinS+(gamSChemo-gamMinS)*bS/(bS+G)
+				gam0v=gamMin0+(gam0-gamMin0)*bn/(bn+G)
+				etaNPv=etaNPChemo+(etaMaxNP-etaNPChemo)*G/(cn+G)
+				tauNMv=tauNMmax/(1+(Vmax-1)*G/(bv+G))
+				tauNv=tauNMv+tauNP
+				I0c=0  # these here since both for negative times and when not infusing
+				I0g=0
+				if (Time < 0) {
+					delEtaNP=etaNP*tauNP
+					delGam0=gam0*tauNMmax
+					delGamS=gamS*tauS
+					An=exp(delEtaNP - delGam0)
+					Aq=2*exp(-delGamS)
+					dQ=-(k0/(1+(Q/the2)^s2) + f0/(1+(N/the1)^s1)+kdel)*Q + Aq*k0/(1+(Qss/the2)^s2)*Qss
+					dN=-gamN*N + An*f0/(1+(Nss/the1)^s1)*Qss
+					dC=0;	dX=0;	dG=0; trt=FALSE
+				}	else {
+					trt=(Time%/%T  < cycles)
+					if (!is.na(T)&&trt&&(Time%%T < Delc)) I0c=Dc/Delc # in chemo infusion period
+					if ( !is.na(T1)&&trt&&(Time%%T -T1 > 0)&&(Time%%T -T1 < Delg) ) I0g=Dg/Delg #in G-CSF injection period
+					delEtaNP=lagvalue(Time - tauNMv)[3]-lagvalue(Time - tauNv)[3]
+					delGam0=Gam0 -lagvalue(Time - tauNMv)[4]
+					delGamS=GamS -lagvalue(Time - tauS)[5]
+					An=exp(delEtaNP - delGam0)
+					Aq=2*exp(-delGamS)
+					Qts=lagvalue(Time - tauS)[1]
+					Qtn=lagvalue(Time - tauNv)[1]
+					Ntn=lagvalue(Time - tauNv)[2]
+					dQ=-(k0/(1+(Q/the2)^s2) + f0/(1+(N/the1)^s1)+kdel)*Q + Aq*k0/(1+(Qts/the2)^s2)*Qts
+					dN=-gamN*N + An*f0/(1+(Ntn/the1)^s1)*Qtn
+					dC= I0c/phi - del*C
+					dX= I0g + kT*VB*G -kB*X
+					dG= Gprod - kT*G + kB*X/VB -gamG*G - sig*N*G^2/(kG+G^2)
+				}
+				dEtaNP=etaNPv
+				dGam0=gam0v
+				dGamS=gamSv
+				list(c(dQ,dN,dEtaNP,dGam0,dGamS,dG,dX,dC),
+						c(delEtaNP=delEtaNP,delGam0=delGam0,delGamS=delGamS,
+								etaNPv=etaNPv,gam0v=gam0v,gamSv=gamSv,trt=trt,tauNMv=tauNMv,tauNv=tauNv,An=An,Aq=Aq))
+			})
+}
+
+
+
--- myelo/inst/doc/brooksPars.R
+++ myelo/inst/doc/brooksPars.R
@@ -4,46 +4,59 @@
 #Journal of TheoreticalBiology 315(2012) 97–109
 
 brooksPars=c(
-		Qss=1.12e6, #cells/kg
+# Stem cell params
+		Qss=1120693, #cells/kg
 		gamS=0.1043, # 1/day
 		gamMinS=0.03,
-		gamMaxS=0.40, 
+		gamMaxS=0.40,   # NOT USED  ###############
 		tauS=2.83, #days1,2
 		k0=8.0, # days
-		the2=0.0826e6, # cells/kg
+		the2=0.0826e6, # cells/kg  the=theta
 		s2=2,
-		Nss=5.59e8, # cells/kg
+# Neutrophil params
+		Nss=559290277, # cells/kg
 		gamN=2.4, # 1/day
-bn=0.05, # mg/ml =b0   # not indented = param not in Zhuge et al
-		tauNP=5.9, # days       Note: many values changed
+		bn=0.05, # mg/ml NOTE: This is b0 in Eq. 16  
+		tauNP=5.9, # days       
 		tauNMmax=3.8, # days
 		tauN=9.7, # days
-bv=0.001, # ug/ml 
-Vmax=3.8,
+		bv=0.001, # ug/ml 
+		Vmax=3.8,
 		etaNP=2.1995, # 1/days
 		etaMinNP=0.4, # 1/days
 		etaMaxNP=2.5444, # 1/days
-AN=1549.58e2,
+		AN=1549.58e2,   # NOT USED, COMPUTED FROM OTHER PARAMS
 		gam0=0.27, # 1/day
 		gamMin0=0.12, 
 		f0=0.154605, # 1/days
 		the1=0.0154848e8, # cells/kg 
 		s1=.5, 
-		kdel=0.0134, # 1/days
-Xss=0.1, # ug/kg
-Gss=0, # ug/ml 
-VB=76, # ml/kg
-Gprod=0, # ug/(mlxday)
-kT=1.68, # 1/days
-kB=6.4,# 1/days
-sig=0.72,# kg/day
-gamG=4.36,# days
-kG=10, #(ug/ml)^2   
-#Chemotherapy
-del=100, # 1/days
-phi=32.07, #1/days
-hS=0.0702, #kg/(mgxday)
-hNP=0.4275 #kg/(mgxday)
+		kdel=0.0134, # 1/days   increased >10-fold in Figure 5 to destabilize
+# G-CSF params
+		Xss=0.1, # ug/kg   SHOULDN'T THIS BE ZERO?????
+		Gss=0, # ug/ml 
+		VB=76, # ml/kg
+		Gprod=0, # ug/(mlxday)
+		kT=1.68, # 1/days
+		kB=6.4,# 1/days
+		sig=0.72,# kg/day
+		gamG=4.36,# days
+		kG=10, #(ug/ml)^2   
+		T1=5, # figure 9 shows this is the best time for GCSF in days after chemo starts
+		Delg=1, # spread GCSF injection of 1 day to help numerics (i.e. need samples at higher res
+		Dg=0,  # default is no GCSF, as in Figures 3 and 5
+#Chemotherapy params
+		del=100, # 1/days
+		phi=32.07, #1/days
+		hS=0.0702, #kg/(mgxday)
+		hNP=0.4275, #kg/(mgxday)
+		cycles=1, # stop after one cycle of therapy
+		Delc=1, # spread chemo injection over 1 day to help numerics
+		Dc=135, # chemo/taxol dose of 135 mg/kg 
+		T=21, # default cycle length = 21 days
+		bS=0.01, # not in pdf, but in C++ code
+		cn=0.085 # same here
 )
 
 save(brooksPars,file="brooksPars.rda")
+
