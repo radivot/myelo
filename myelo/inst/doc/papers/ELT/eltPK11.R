84e08d5e2050f423ebeb8fb2d4cef595243e4ebe
Tom Radivoyevitch <radivot@gmail.com>
2017-12-18 16:48:55 -0500

    added comments

--- myelo/inst/doc/papers/ELT/eltPK11.R
+++ myelo/inst/doc/papers/ELT/eltPK11.R
@@ -2,11 +2,14 @@ library(myelo)
 library(deSolve)
 library(rodeoExt)
 # Pharmacokinetics and Hepatic Uptake of Eltrombopag, a Novel
-# Platelet-Increasing Agent□S Kazuya Takeuchi, Tomoko Sugiura, Saki Umeda,
+# Platelet-Increasing Agent,   S Kazuya Takeuchi, Tomoko Sugiura, Saki Umeda,
 # Kazuki Matsubara, Masato Horikawa, Noritaka Nakamichi, David L. Silver,
 # Norihisa Ishiwata, and Yukio Kato, DRUG METABOLISM AND DISPOSITION Copyright
 # 2011 by The American Society for Pharmacology and Experimental Therapeutics
-# DMD 39:1088–1096, 2011
+# DMD 39:1088–1096, 2011    # ELT has a COO- group, and rifampicin blocks anion transport into liver
+
+#IV in rats, 40% out in bile unchanged at 72 hours
+
 # table 1
 CLtot=33.3  #ml/(kg*h)  
 CLbile=13.6  #ml/(kg*h)  [0 to 72 hours]
