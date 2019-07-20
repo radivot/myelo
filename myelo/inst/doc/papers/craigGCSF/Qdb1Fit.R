1cee2a5f5e56bbed4f853c4c5a65280b2406e775
Tom Radivoyevitch <radivot@gmail.com>
2019-07-20 19:24:38 -0400

    days of CT vs betaSS

--- myelo/inst/doc/papers/craigGCSF/Qdb1Fit.R
+++ myelo/inst/doc/papers/craigGCSF/Qdb1Fit.R
@@ -80,16 +80,11 @@ summary(M<-mle2(nLL,method="Nelder-Mead",
                  control = list(maxit=50000, parscale=IC[fit]) ) )
 # 
 # Coefficients:
-#   Estimate Std. Error  z value     Pr(z)    
-#   Q0     1.1021e+00 3.6840e-04 2991.637 < 2.2e-16 ***
-#   beta0  9.9462e-02 2.9259e-04  339.931 < 2.2e-16 ***
-#   betaSS 4.3025e-02 2.2473e-05 1914.491 < 2.2e-16 ***
-#   sigma  1.9685e-03 6.0940e-05   32.303 < 2.2e-16 ***
-#   ---
-#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-# 
-# -2 log L: -5013.127 
-
+#   Estimate Std. Error z value     Pr(z)    
+#   Q0     1.09904381 0.00181971 603.966 < 2.2e-16 ***
+#   beta0  0.09636617 0.00131101  73.505 < 2.2e-16 ***
+#   betaSS 0.04290271 0.00011098 386.564 < 2.2e-16 ***
+#   sigma  0.00972905 0.00030149  32.269 < 2.2e-16 ***
 
 psim=function(Q0,beta0,betaSS,sigma) {
   (x0=c( Q = Q0, Aq = Aqss))
@@ -102,7 +97,7 @@ psim=function(Q0,beta0,betaSS,sigma) {
 }
 
 
-signif(coef(summary(M))[,1:2],3)
+signif(coef(summary(M))[,1:2],4)
 round(coef(M),5)
 p=as.list(coef(M))
 out=do.call(psim,p)
@@ -111,4 +106,103 @@ gy=ylab(quote(paste(10^6," HSC/(kg body mass)")))
 d%>%ggplot(aes(x=t,y=Q))+geom_point(size=0.3)+geom_line(aes(x=t,y=ei),size=.3)+gx+gy+tc(14)
 ggsave("~/Results/myelo/Qdb1_2x6fit.pdf",width=4, height=4) 
 
+###### add in threshold estimate
+nLLt<-function(Q0,thresh,beta0,betaSS,sigma) { # pass these globally: d,eventsdat,Aqss
+  (x0=c( Q = Q0, Aq = Aqss))
+  kapDel=(Aqss-1)*betaSS
+  parsQdb1=c(fQ = beta0, thresh = thresh, betaSS = betaSS, kapDel = kapDel)
+  out=dede(x0,times = d$t, func = "derivsQdb1",	parms = parsQdb1,
+           dllname = "myelo",initfunc = "parmsQdb1",
+           events=list(data=eventsdat),method="lsoda",  
+           nout = 1, outnames = c("beta"))  
+  # -sum(dnorm(log(d$Q), mean=log(out[,"Q"]),sigma,log=TRUE))
+  -sum(dnorm(d$Q, mean=out[,"Q"],sigma,log=TRUE)) #gets sigma estimate right at 0.002
+}
 
+IC=c(Q0=1.1,thresh=0.1,beta0=0.1,betaSS=0.04303868,sigma=0.01) # leaves at initial optimum
+IC=c(Q0=.8,thresh=0.2,beta0=0.07,betaSS=0.03,sigma=0.02) # recreates opt above used to make data
+IC=c(Q0=2,thresh=0.1,beta0=0.1,betaSS=0.04303868,sigma=0.01) # leaves at initial optimum
+fit=c(1:5)
+summary(M<-mle2(nLLt,method="Nelder-Mead",
+                fixed=as.list(IC)[-fit],
+                start=as.list(IC),data=list(d=d),
+                control = list(maxit=50000, parscale=IC[fit]) ) )
+
+psimt=function(Q0,thresh,beta0,betaSS,sigma) {
+  (x0=c( Q = Q0, Aq = Aqss))
+  kapDel=(Aqss-1)*betaSS
+  parsQdb1=c(fQ = beta0, thresh = thresh, betaSS = betaSS, kapDel = kapDel)
+  data.frame(dede(x0,times = d$t, func = "derivsQdb1",	parms = parsQdb1,
+                  dllname = "myelo",initfunc = "parmsQdb1",
+                  events=list(data=eventsdat),method="lsoda",  
+                  nout = 1, outnames = c("beta"))) 
+}
+
+signif(coef(summary(M))[,1:2],4)
+round(coef(M),5)
+p=as.list(coef(M))
+out=do.call(psimt,p)
+d$ei=out[,"Q"]
+gy=ylab(quote(paste(10^6," HSC/(kg body mass)")))
+d%>%ggplot(aes(x=t,y=Q))+geom_point(size=0.3)+geom_line(aes(x=t,y=ei),size=.3)+gx+gy+tc(14)
+ggsave("~/Results/myelo/Qdb1_2x6fitThresh.pdf",width=4, height=4) 
+
+
+###### add in chemo time 
+nLLtCT<-function(Q0,thresh,beta0,betaSS,sigma,CT) { # pass these globally: d,eventsdat,Aqss
+  Q0=exp(Q0);thresh=exp(thresh);beta0=exp(beta0);betaSS=exp(betaSS);sigma=exp(sigma);CT=exp(CT)
+  (StrTimes=seq(0,80,14))
+  (StpTimes=StrTimes+CT)
+  nc=length(StpTimes)
+  (events=tibble(var=rep("Aq",2*nc),
+                 time=sort(c(StrTimes,StpTimes)),
+                 value=rep(c(0.0*parsQ["Aqss"],parsQ["Aqss"]),nc),
+                 method=rep("rep",2*nc)))
+  events2=events
+  events2$time=events2$time+150
+  (eventsdat=as.data.frame(bind_rows(events,events2)))
+  (x0=c( Q = Q0, Aq = Aqss))
+  kapDel=(Aqss-1)*betaSS
+  parsQdb1=c(fQ = beta0, thresh = thresh, betaSS = betaSS, kapDel = kapDel)
+  out=dede(x0,times = d$t, func = "derivsQdb1",	parms = parsQdb1,
+           dllname = "myelo",initfunc = "parmsQdb1",
+           events=list(data=eventsdat),method="lsoda",  
+           nout = 1, outnames = c("beta"))  
+  # -sum(dnorm(log(d$Q), mean=log(out[,"Q"]),sigma,log=TRUE))
+  -sum(dnorm(d$Q, mean=out[,"Q"],sigma,log=TRUE)) #gets sigma estimate right at 0.002
+}
+
+IC=c(Q0=1.1,thresh=0.1,beta0=0.1,betaSS=0.04303868,sigma=0.01,CT=5) # leaves at initial optimum
+IC=c(Q0=.8,thresh=0.1,beta0=0.07,betaSS=0.03,sigma=0.01,CT=4.9) # recreates opt above used to make data
+IC=c(Q0=2,thresh=0.1,beta0=0.1,betaSS=0.04303868,sigma=0.01,CT=5) # leaves at initial optimum
+fit=c(1:5)
+IC=log(IC)
+summary(M<-mle2(nLLtCT,method="Nelder-Mead",
+                fixed=as.list(IC)[-fit],
+                start=as.list(IC),data=list(d=d),
+                control = list(maxit=50000, parscale=IC[fit]) ) )
+signif(exp(coef(summary(M))[,1:2]),4)
+round(coef(M),5)
+
+# 
+# 
+# psimt=function(Q0,thresh,beta0,betaSS,sigma) {
+#   (x0=c( Q = Q0, Aq = Aqss))
+#   kapDel=(Aqss-1)*betaSS
+#   parsQdb1=c(fQ = beta0, thresh = thresh, betaSS = betaSS, kapDel = kapDel)
+#   data.frame(dede(x0,times = d$t, func = "derivsQdb1",	parms = parsQdb1,
+#                   dllname = "myelo",initfunc = "parmsQdb1",
+#                   events=list(data=eventsdat),method="lsoda",  
+#                   nout = 1, outnames = c("beta"))) 
+# }
+# 
+# 
+# 
+# p=as.list(coef(M))
+# out=do.call(psimt,p)
+# d$ei=out[,"Q"]
+# gy=ylab(quote(paste(10^6," HSC/(kg body mass)")))
+# d%>%ggplot(aes(x=t,y=Q))+geom_point(size=0.3)+geom_line(aes(x=t,y=ei),size=.3)+gx+gy+tc(14)
+# ggsave("~/Results/myelo/Qdb1_2x6fitThreshCT.pdf",width=4, height=4) 
+# 
+# 
